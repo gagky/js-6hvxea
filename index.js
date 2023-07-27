086634986c5fb1b400f7e7f15d7f4286b276eadd
gagky <rocky.cuhk@gmail.com>
2023-07-27 16:16:15 +1000

    1 file modified

--- index.js
+++ index.js
@@ -7,146 +7,13 @@ import './CommonMixin.js';
 import { AirSearchAnalysis } from './AirSearchAnalysis.js';
 
 const airSearchAnalysis = new AirSearchAnalysis(exmapleJson);
-// console.log('anything', airSearchAnalysis.findItineraries());
 
-// console.log(
-//   'with 3593',
-//   airSearchAnalysis.findItineraries({
-//     flightSegments: [
-//       {
-//         flightNumber: '3593',
-//       },
-//     ],
-//   })
-// );
-// console.log(
-//   'with 1531 & 557',
-//   airSearchAnalysis.findItineraries({
-//     flightSegments: [
-//       {
-//         flightNumber: '1531',
-//       },
-//       {
-//         flightNumber: '557',
-//       },
-//     ],
-//   })
-// );
-// console.log(
-//   'with VA 1531 & VA 557',
-//   airSearchAnalysis.findItineraries({
-//     flightSegments: [
-//       {
-//         flightNumber: '1531',
-//         marketingAirlineRef: 'VA',
-//       },
-//       {
-//         flightNumber: '557',
-//         marketingAirlineRef: 'VA',
-//       },
-//     ],
-//   })
-// );
+const AnalysisRender = (title, result) => {
+  document.write(`<h1>${title}</h1>`);
+  document.write(`<pre><code>${result}</code></pre>`);
+};
 
-// console.log(
-//   'with VA 1531 & VA 557 / originDestinationGroup.index',
-//   airSearchAnalysis.findItineraries({
-//     originDestinationGroup: { index: 0 },
-//     flightSegments: [
-//       {
-//         flightNumber: '1531',
-//         marketingAirlineRef: 'VA',
-//       },
-//       {
-//         flightNumber: '557',
-//         marketingAirlineRef: 'VA',
-//       },
-//     ],
-//   })
-// );
-//
-// console.log(
-//   'with VA 1531 & VA 557 / originDestinationGroup.index / departureDateGroup.date',
-//   airSearchAnalysis.findItineraries({
-//     originDestinationGroup: { index: 0 },
-//     departureDateGroup: { date: '2023-06-05' },
-//     flightSegments: [
-//       {
-//         flightNumber: '1531',
-//         marketingAirlineRef: 'VA',
-//       },
-//       {
-//         flightNumber: '557',
-//         marketingAirlineRef: 'VA',
-//       },
-//     ],
-//   })
-// );
-
-/** Deliverable */
-const targetItinerary = airSearchAnalysis.findItineraries({
-  originDestinationGroup: {
-    index: 0,
-    departureDateGroup: { date: '2023-06-05' },
-  },
-  flightSegments: [
-    {
-      flightNumber: '1531',
-      marketingAirlineRef: 'VA',
-    },
-    {
-      flightNumber: '557',
-      marketingAirlineRef: 'VA',
-    },
-  ],
-})[0];
-console.log('findItineraries', targetItinerary);
-
-const targetItineraryUri = _.getUri(targetItinerary);
-
-console.log('findItineraries -> uri', targetItineraryUri);
-
-// console.log(
-//   'find resources by targetItineraryUri ',
-//   _.chain(airSearchAnalysis.json).findResourcesByUri(targetItineraryUri).value()
-// );
-
-// console.log(
-//   'find parent by targetItineraryUri',
-//   _.chain(airSearchAnalysis.json)
-//     .findParentByReferenceUri(targetItineraryUri)
-//     .value()
-// );
-
-// console.log(
-//   'find parent by targetItineraryUri with farePrice',
-//   _.chain(airSearchAnalysis.json)
-//     .findParentByReferenceUri(targetItineraryUri, ['farePrice'])
-//     .value()
-// );
-
-// console.log(
-//   'find parent by targetItineraryUri with farePrice, map uri',
-//   _.chain(airSearchAnalysis.json)
-//     .findParentByReferenceUri(targetItineraryUri, ['farePrice'])
-//     .mapResourceUris()
-//     .value()
-// );
-
-// const itineraryPrices =
-//   airSearchAnalysis.findItineraryPrices(targetItineraryUri);
-// console.log('findItineraryPrices', itineraryPrices);
-
-// const itineraryPricesUris = _.mapResourceUris(itineraryPrices);
-// console.log('findItineraryPrices -> uri', itineraryPricesUris);
-/** Deliverable */
-const itineraryPricesOfBu = airSearchAnalysis.listItineraryPricesByItineraryUri(
-  targetItineraryUri,
-  { fareFamilyRef: 'BU' }
-);
-console.log('listItineraryPricesByItineraryUri', itineraryPricesOfBu);
-
-console.log(
+AnalysisRender(
   '1531+557',
   airSearchAnalysis.findItineraryPrices({
     originDestinationGroup: {
@@ -165,7 +32,7 @@ console.log(
     ],
   })
 );
-console.log(
+AnalysisRender(
   '1531+557 / BU',
   airSearchAnalysis.findItineraryPrices({
     originDestinationGroup: {
@@ -186,7 +53,7 @@ console.log(
   })
 );
 
-console.log(
+AnalysisRender(
   '1531+557 / CH',
   airSearchAnalysis.findItineraryPrices({
     originDestinationGroup: {
@@ -207,4 +74,4 @@ console.log(
   })
 );
 
-document.write('read console');
+document.write('done');
